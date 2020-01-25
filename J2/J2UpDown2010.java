aed8114082e0bd602491a17c1307e67dddc903b5
JohnstonLiu <johnstonliu2004@gmail.com>
2020-01-24 20:11:02 -0800

    J2 UpDown

---
+++ J2/J2UpDown2010.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+import java.util.*;
+
+public class J2UpDown2010 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
---
+++ J4/J4CrossSpiral2005.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+import java.util.*;
+
+public class J4CrossSpiral2005 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
