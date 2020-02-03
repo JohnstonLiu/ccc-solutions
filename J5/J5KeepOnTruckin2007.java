8f71f6fe82d9241a6bcb7eef66d6c6e045ac2023
JohnstonLiu <johnstonliu2004@gmail.com>
2020-02-03 09:52:23 -0800

    J5 Keep On Truckin

---
+++ J5/J5KeepOnTruckin2007.java
@@ -0,0 +1,9 @@
+package PACKAGE_NAME;
+
+import java.util.*;
+
+public class J5KeepOnTruckin2007 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+}
