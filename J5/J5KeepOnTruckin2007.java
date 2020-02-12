3e951d1c0fe32ade5e22addd1e3d0d71d35450b3
Johnston Liu <johnstonliu2004@gmail.com>
2020-02-11 20:35:52 -0800

    J5 Keep On Truckin AC

--- J5/J5KeepOnTruckin2007.java
+++ J5/J5KeepOnTruckin2007.java
@@ -1,59 +1,24 @@
 import java.util.*;
 
-public class J5KeepOnTruckin2007 {
+public class J5KeepOnTruckin2007{
     static List<Integer> motels = new ArrayList<>();
-    static int c = 0;
-    static int min, max;
-    public static void main(String[] args) {
+    public static void main(String[]args) {
+        Collections.addAll(motels,0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000);
         Scanner sc = new Scanner(System.in);
-        Collections.addAll(motels, 0, 990, 1010, 1970, 2030, 2940, 3060, 3930, 4060, 4970, 5030, 5990, 6010, 7000);
-        min = sc.nextInt();
-        max = sc.nextInt();
-        int d = sc.nextInt();
-
-        for (int i = 0; i < d; i++){
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int m = sc.nextInt();
+        for(int i = 0; i < m; i++){
             motels.add(sc.nextInt());
         }
-
-        if (d == 20 && min == 1 && max == 7000){
-            System.out.println("4294967296");
-            return;
-        }
-
-        //if (d == 20){
-        //    System.out.println("4294967295");
-         //   return;
-        //}
+        long dp[] = new long[motels.size()]; dp[0] = 1;
         Collections.sort(motels);
-        foo(0);
-        //System.out.println("ans: "+c);
-        System.out.println(c);
-    }
-
-    public static void foo(int prev){
-        if (motels.get(prev) == 7000){
-            c++;
-            //System.out.println("added");
-        }
-
-        System.out.println(motels.get(prev));
-        int p = motels.get(prev);
-        for (int i = prev + 1; i < motels.size(); i++){
-            int v = motels.get(i);
-            if (isValid(p, v)){
-                foo(i);
-            }else if (v - p > max){
-                //System.out.println("backtrack");
-                return;
+        for(int i = 1; i < motels.size(); i++) {
+            for(int j = 0; j < i; j++) {
+                int valid = motels.get(i) - motels.get(j);
+                if(valid >= a && valid <= b) dp[i] += dp[j];//dp tracks the number of paths that have crossed over the certain motel; we add to the valid next motel our previous branches because they can all go there
             }
         }
-    }
-
-    public static boolean isValid(int prev, int value){
-        //System.out.println("p: "+ prev);
-        //System.out.println("v: " + value);
-        //FSystem.out.println();
-        return value - prev >= min && value - prev <= max;
+        System.out.println(dp[motels.size() - 1]);//we print the number of paths that reached 7000, our destination
     }
 }
-
